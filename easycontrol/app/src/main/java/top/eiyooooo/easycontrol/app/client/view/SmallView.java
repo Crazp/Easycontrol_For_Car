2e16e81f30d6044e206fb01bb523c98c2a6b0ae9
Crazp <58839741+Crazp@users.noreply.github.com>
2025-07-20 02:51:07 +0800

    Update SmallView.java

--- easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/client/view/SmallView.java
+++ easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/client/view/SmallView.java
@@ -229,6 +229,7 @@ public class SmallView extends ViewOutlineProvider {
     setButtonListener(clientView.controlPacket);
     setKeyEvent(clientView.controlPacket);
     // 显示
+    smallViewParams.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
     AppData.windowManager.addView(smallView.getRoot(), smallViewParams);
     smallView.textureViewLayout.addView(clientView.textureView, 0);
     clientView.viewAnim(smallView.getRoot(), true, 0, PublicTools.dp2px(40f), null);
